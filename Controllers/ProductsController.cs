64dab1ec80d2bda0f8de0cc482057d2b480362ee
Esteban Canepa <estebancanepa@gmail.com>
2020-07-09 13:37:58 -0300

    First commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        /// <summary>
+        /// Devuelve los products
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            var products = await Task.Run(() => new List<Product> { new Product { Id = 1, Name = "Arroz", Price = 12.5M } });
+            return Ok(products);
+        }
+    }
+}
